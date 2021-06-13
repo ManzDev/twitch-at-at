b9d2f4bfd57669496daf1a47454cbd5bd3b0041f
ManzDev <JoseRomanHernandez@gmail.com>
2021-06-13 23:09:50 +0100

    Twitch (live)

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// CSS.registerProperty({
+//   name: "--foot-rotate",
+//   syntax: "<angle>",
+//   inherits: true,
+//   initialValue: "0deg"
+// });
+
+// CSS.registerProperty({
+//   name: "--knee-rotate",
+//   syntax: "<angle>",
+//   inherits: true,
+//   initialValue: "0deg"
+// });
